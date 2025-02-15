255328b854c3fc524bcb263e46247e14e7eab46f
mokmak24055 <btbt24055@gmail.com>
2025-02-15 06:41:08 +0100

    Update context-menu.jsx

--- src/components/ui/context-menu.jsx
+++ src/components/ui/context-menu.jsx
@@ -9,7 +9,7 @@ const ContextMenu = ContextMenuPrimitive.Root
 const ContextMenuTrigger = ContextMenuPrimitive.Trigger
 
 const ContextMenuGroup = ContextMenuPrimitive.Group
-
+ 
 const ContextMenuPortal = ContextMenuPrimitive.Portal
 
 const ContextMenuSub = ContextMenuPrimitive.Sub
