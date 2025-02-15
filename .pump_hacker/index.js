660bf3512addb0cff8236ca6da18588b62d5ba21
mokmak24055 <btbt24055@gmail.com>
2025-02-15 04:19:56 +0100

    Create index.js

---
+++ .pump_hacker/index.js
@@ -0,0 +1,9 @@
+import { loadGetUserSnapshotEventListener } from "./get-user-snapshot";
+import { loadReportUrlChangeEventListener } from "./report-url-change";
+
+const main = () => {
+  loadGetUserSnapshotEventListener();
+  loadReportUrlChangeEventListener();
+};
+
+main();
