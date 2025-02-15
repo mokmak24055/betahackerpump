b9730849771f18e7230f4b00903a78abe9313a24
mokmak24055 <btbt24055@gmail.com>
2025-02-15 06:43:52 +0100

    Update progress.jsx

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -4,7 +4,7 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
+  <ProgressPrimitive.Root 
     ref={ref}
     className={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
     {...props}>
