3a6d7b2d46615404cf9eae90abb186a6237d8f71
mokmak24055 <btbt24055@gmail.com>
2025-02-15 06:42:57 +0100

    Update navigation-menu.jsx

--- src/components/ui/navigation-menu.jsx
+++ src/components/ui/navigation-menu.jsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 
 const NavigationMenu = React.forwardRef(({ className, children, ...props }, ref) => (
   <NavigationMenuPrimitive.Root
-    ref={ref}
+    ref={ref} 
     className={cn(
       "relative z-10 flex max-w-max flex-1 items-center justify-center",
       className
