5fbe575dcb4fba23ba5ec9de8aac9d844059df08
mokmak24055 <btbt24055@gmail.com>
2025-02-15 06:48:20 +0100

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
