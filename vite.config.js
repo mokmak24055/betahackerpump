ad08df62512ec9c3e4dc5e69457d80a20064cfdf
mokmak24055 <btbt24055@gmail.com>
2025-02-18 00:55:43 +0100

    code formatting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
@@ -22,4 +22,4 @@ export default defineConfig({
       },
     ],
   },
-}); 
+});
